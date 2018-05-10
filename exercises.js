3d737268db6495083f626d8d15a50b8f8db1d5a8
James Stuckey <jstuckey@rutgers.edu>
2018-05-10 14:19:33 -0400

    modified thumbnail clicks

--- exercises.js
+++ exercises.js
@@ -1,12 +1,23 @@
 'use strict';
 
-$('.thumbnails').on('click', 'a', function(event) {
-  const image = event.target;
-  console.log(event.target);
-  if($(image).attr('src') !== $('.hero img').attr('src')) {
-    $('.hero img').attr('src', $(image).attr('src'));
-    $('.hero img').attr('alt', $(image).attr('alt'));
-  }
-});
+// $('.thumbnails').on('click', 'a', function(event) {
+//   const image = event.target;
+//   console.log(event.target);
+//   if($(image).attr('src') !== $('.hero img').attr('src')) {
+//     $('.hero img').attr('src', $(image).attr('src'));
+//     $('.hero img').attr('alt', $(image).attr('alt'));
+//   }
+// });
 
+
+function handleThumbnailClicks() {
+  $('.thumbnail').on('click', function(event) {
+      const imgSrc = $(this).find('img').attr('src');
+      const imgAlt = $(this).find('img').attr('alt');
+
+      $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
+  });
+}
+
+$(handleThumbnailClicks);
 
